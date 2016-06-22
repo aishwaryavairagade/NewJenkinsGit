23079a58409c45b6327444be833585a05177c19c
aishwaryavairagade <aishwaryav@cybage.com>
2016-06-22 14:44:06 +0530

    Initial commit

---
+++ src/DateTime.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTime{
+	public static void main(String args[]) {
+	    
+	       Date date = new Date();
+	        
+	       System.out.println(date.toString());
+
+}
+}

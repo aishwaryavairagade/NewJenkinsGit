06835486690bab9ef6c7e671369a5fad7ac68560
aishwaryavairagade <aishwaryav@cybage.com>
2016-06-22 14:50:27 +0530

    Commit changes
    Commit changes
    
    New Commit changes

--- src/DateTime.java
+++ src/DateTime.java
@@ -2,11 +2,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateTime{
-	public static void main(String args[]) {
-	    
-	       Date date = new Date();
-	        
-	       System.out.println(date.toString());
-
+public static void main(String args[]){
+ Date date = new Date();
+   //printing date and time
+  SimpleDateFormat ft = new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+  
+  System.out.println(date);
+  //printing date
+  System.out.println("Current Date: " + ft.format(date));
 }
 }
